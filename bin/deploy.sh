38e668d1b9ac845f859acfb272cf4949d3731a4e
simplonco <simplonco@buis.simplonco>
2014-11-03 18:36:33 +0100

    Auto Commit

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,10 +5,9 @@ DIRECTION="git@heroku.com:acacias.git"
 echo "********** Déploiement **********"
 
 heroku login && \
-cd $DIRECTION
 git add . && \
 git commit -m "Auto Commit" && \
-git push heroku origin master && \
+git push $DIRECTION origin master && \
 heroku run rake db:migrate
 
 echo "********** OK **********"
